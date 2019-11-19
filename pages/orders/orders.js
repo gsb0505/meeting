3dafbb42a716e60da296d41c04e1f9e8f48cf11b
gsb0505 <15168268827@139.com>
2019-11-19 20:12:54 +0800

    add

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -1,9 +1,6 @@
 // pages/orders/orders.js
 const request = require('../../utils/request.js')
-const Parser = require('../../utils/dom-parser.js')
-const dom = require('../../utils/dom.js')
-const entities = require('../../utils/entities.js')
-const sax = require('../../utils/sax.js')
+const X2JS = require('../../utils/we-x2js.js')
 var app = getApp()
 const page_size = 5;
 Page({
@@ -12,6 +9,11 @@ Page({
     hidden: false,
     prompt: true,
     ordersTitle: '暂无预定',
+    col0:'',
+    col1:'',
+    col2:'',
+    col3:'',
+    col4:'',
     // 当前页
     pageNumber: 1,
     // 总页数
@@ -28,7 +30,7 @@ Page({
     // getApp().auth();
     // 页面初始化 options为页面跳转所带来的参数
       this.setData({
-        colLeft:'#be9d6d',
+        col0:'#be9d6d',
         borLeft:'3px solid #be9d6d'
       });
   },
@@ -36,25 +38,72 @@ Page({
    
     this.setData({
       pageNumber: 1,
-      room: []
+      orderDetailList: []
     }),
-      this.queryData()
+      this.queryData(null)
   },
 
-  queryData: function () {
+  
+  tabRight: function (e) {
+    let id = e.currentTarget.id;
+    let col0 = '';
+    let col1 = '';
+    let col2 = '';
+    let col3 = '';
+    let col4 = '';
+    let left = '1%';
+    if (id == 0){
+      col0 ='#be9d6d';
+      left='1%';
+    }else if (id == 1 ){
+      col1 = '#be9d6d';
+      left = '21%';
+    } else if (id == 2) {
+      col2 = '#be9d6d';
+      left = '61%';
+    } else if (id == 3) {
+      col3 = '#be9d6d';
+      left = '81%';
+    } else if (id == 4) {
+      col4 = '#be9d6d';
+      left = '41%';
+    } 
+
+    // 点击会议室 
+    this.setData({
+      col0: col0,
+      col1: col1,
+      col2: col2,
+      col3: col3,
+      col4: col4,
+      Left: left,
+      pageNumber: 1,
+      orderDetailList: []
+    });
+    if (id == 0){
+      this.queryData(null);
+    }else{
+      this.queryData(id);
+    }
+  } ,
+  queryData: function (errCode) {
     const that = this
     let userId = app.globalData.userId;
-    request.meetList({creator: userId, currentPage: this.data.pageNumber, pageSize: page_size }, function (res) {
-      debugger
-      var XMLParser = new Parser.DOMParser()
-      var curData = XMLParser.parseFromString(res.data)
-      // let curData = JSON.parse(res.data)
+    request.meetList({ creator: userId, currentPage: this.data.pageNumber, errCode: errCode, pageSize: page_size }, function (res) {
       //debugger
-      let arr = curData.getElementsByTagName('orderDetails').childNodes;
+      var x2js = new X2JS();
+      let orderDetails = x2js.xml2js(res.data)
+      let orderDetailList = orderDetails == null || orderDetails == '' || typeof (orderDetails) == 'undefined' ? '' : orderDetails.orderDetails.orderDetail;
+      debugger
+      let totalPage = orderDetailList == null ? 1 : orderDetailList[0] == null ? orderDetailList.pageCount.totalPage : orderDetailList[0].pageCount.totalPage;
+      let totalResult = orderDetailList == null ? 0 : orderDetailList[0] == null ? orderDetailList.pageCount.totalResult : orderDetailList[0].pageCount.totalResult;
+      
       that.setData({
-        tables: arr,
-        // totalPage: arr.totalPage,
-        // totalResult: curData.totalResult,
+        orderDetailList: typeof (orderDetailList) == 'undefined' ? '' :  orderDetailList,
+        prompt: typeof (orderDetailList) == 'undefined' ? false:true,
+        ordersTitle: '暂无预定会议室',
+        totalPage: totalPage,
+        totalResult: totalResult,
         stopLoadMoreTiem: false
       })
     })
